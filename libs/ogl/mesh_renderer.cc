c699c93bab13b167dc971d68a18d5bcc40162b32
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-03-19 11:32:44 -0700

    Merge vertex_buffer, vertex_array, mesh_renderer into single mesh_renderer.h/.cc

--- libs/ogl/mesh_renderer.cc
+++ libs/ogl/mesh_renderer.cc
@@ -7,10 +7,107 @@
  * of the BSD 3-Clause license. See the LICENSE.txt file for details.
  */
 
-#include "ogl/ogl_common.h"
+#include <stdexcept>
+
 #include "ogl/mesh_renderer.h"
 
 OGL_NAMESPACE_BEGIN
+
+/* ---- VertexBuffer ---- */
+
+VertexBuffer::VertexBuffer (void)
+{
+    glGenBuffers(1, &this->vbo_id);
+    check_gl_error();
+    this->vbo_target = GL_ARRAY_BUFFER;
+    this->datatype = GL_FLOAT;
+    this->vpv = 0;
+    this->elems = 0;
+}
+
+void
+VertexBuffer::set_data (GLfloat const* data, GLsizei elems, GLint vpv)
+{
+    this->vbo_target = GL_ARRAY_BUFFER;
+    this->datatype = GL_FLOAT;
+    this->vpv = vpv;
+    this->elems = elems;
+
+    this->bind();
+    GLsizeiptr bytes = elems * vpv * sizeof(GLfloat);
+    glBufferData(this->vbo_target, bytes, data, GL_STATIC_DRAW);
+    check_gl_error();
+}
+
+void
+VertexBuffer::set_indices (GLuint const* data, GLsizei num_indices)
+{
+    this->vbo_target = GL_ELEMENT_ARRAY_BUFFER;
+    this->datatype = GL_UNSIGNED_INT;
+    this->vpv = 3;
+    this->elems = num_indices;
+
+    this->bind();
+    GLsizeiptr bytes = num_indices * sizeof(GLuint);
+    glBufferData(this->vbo_target, bytes, data, GL_STATIC_DRAW);
+    check_gl_error();
+}
+
+/* ---- VertexArray ---- */
+
+void
+VertexArray::assign_attrib (BoundVBO const& bound_vbo)
+{
+    VertexBuffer::Ptr vbo = bound_vbo.first;
+    std::string const& name = bound_vbo.second;
+
+    GLint location = this->shader->get_attrib_location(name.c_str());
+    if (location < 0)
+        return;
+
+    vbo->bind();
+    glVertexAttribPointer(location, vbo->get_values_per_vertex(),
+        vbo->get_data_type(), GL_TRUE, 0, nullptr);
+    check_gl_error();
+    glEnableVertexAttribArray(location);
+    check_gl_error();
+}
+
+void
+VertexArray::draw (void)
+{
+    if (this->vert_vbo == nullptr)
+        throw std::runtime_error("No vertex VBO set!");
+
+    if (this->shader == nullptr)
+        throw std::runtime_error("No shader program set!");
+
+    glBindVertexArray(this->vao_id);
+    check_gl_error();
+
+    this->shader->bind();
+
+    this->assign_attrib(BoundVBO(this->vert_vbo, OGL_ATTRIB_POSITION));
+
+    for (std::size_t i = 0; i < this->vbo_list.size(); ++i)
+        this->assign_attrib(this->vbo_list[i]);
+
+    if (this->index_vbo != nullptr) {
+        this->index_vbo->bind();
+        glDrawElements(this->primitive, this->index_vbo->get_element_amount(),
+            GL_UNSIGNED_INT, nullptr);
+        check_gl_error();
+    } else {
+        glDrawArrays(this->primitive, 0, this->vert_vbo->get_element_amount());
+        check_gl_error();
+    }
+
+    this->shader->unbind();
+    glBindVertexArray(0);
+    check_gl_error();
+}
+
+/* ---- MeshRenderer ---- */
 
 void
 MeshRenderer::set_mesh (mve::TriangleMesh::ConstPtr mesh)
@@ -18,7 +115,6 @@ MeshRenderer::set_mesh (mve::TriangleMesh::ConstPtr mesh)
     if (mesh == nullptr)
         throw std::invalid_argument("Got null mesh");
 
-    /* Clean previous content. */
     this->reset_vertex_array();
 
     mve::TriangleMesh::VertexList const& verts(mesh->get_vertices());
@@ -27,41 +123,32 @@ MeshRenderer::set_mesh (mve::TriangleMesh::ConstPtr mesh)
     mve::TriangleMesh::ColorList const& vcolors(mesh->get_vertex_colors());
     mve::TriangleMesh::TexCoordList const& vtexuv(mesh->get_vertex_texcoords());
 
-    /* Init vertex VBO. */
     {
         VertexBuffer::Ptr vbo = VertexBuffer::create();
         vbo->set_data(&verts[0][0], (GLsizei)verts.size(), 3);
         this->set_vertex_vbo(vbo);
     }
 
-    /* Init index VBO if faces are given. */
-    if (!faces.empty())
-    {
-        VertexBuffer::Ptr vbo = ogl::VertexBuffer::create();
+    if (!faces.empty()) {
+        VertexBuffer::Ptr vbo = VertexBuffer::create();
         vbo->set_indices(&faces[0], (GLsizei)faces.size());
         this->set_index_vbo(vbo);
     }
 
-    /* Init normal VBO if normals are given. */
-    if (!vnormals.empty())
-    {
-        VertexBuffer::Ptr vbo = ogl::VertexBuffer::create();
+    if (!vnormals.empty()) {
+        VertexBuffer::Ptr vbo = VertexBuffer::create();
         vbo->set_data(&vnormals[0][0], (GLsizei)vnormals.size(), 3);
         this->add_vbo(vbo, OGL_ATTRIB_NORMAL);
     }
 
-    /* Init color VBO if colors are given. */
-    if (!vcolors.empty())
-    {
-        VertexBuffer::Ptr vbo = ogl::VertexBuffer::create();
+    if (!vcolors.empty()) {
+        VertexBuffer::Ptr vbo = VertexBuffer::create();
         vbo->set_data(&vcolors[0][0], (GLsizei)vcolors.size(), 4);
         this->add_vbo(vbo, OGL_ATTRIB_COLOR);
     }
 
-    /* Init UV VBO if texture coordinates are given. */
-    if (!vtexuv.empty())
-    {
-        VertexBuffer::Ptr vbo = ogl::VertexBuffer::create();
+    if (!vtexuv.empty()) {
+        VertexBuffer::Ptr vbo = VertexBuffer::create();
         vbo->set_data(&vtexuv[0][0], (GLsizei)vtexuv.size(), 2);
         this->add_vbo(vbo, OGL_ATTRIB_TEXCOORD);
     }
--- libs/ogl/mesh_renderer.h
+++ libs/ogl/mesh_renderer.h
@@ -10,20 +10,189 @@
 #ifndef OGL_MESH_RENDERER_HEADER
 #define OGL_MESH_RENDERER_HEADER
 
+#include <utility>
+#include <vector>
+#include <string>
 #include <memory>
 
 #include "mve/mesh.h"
 #include "ogl/ogl_common.h"
 #include "ogl/shader_program.h"
-#include "ogl/vertex_array.h"
-#include "ogl/vertex_buffer.h"
 
 OGL_NAMESPACE_BEGIN
 
-/**
- * OpenGL renderer that takes a mesh and automatically creates
- * the appropriate VBOs and a vertex array object.
- */
+/* ---- VertexBuffer ---- */
+
+// OpenGL vertex buffer object (VBO) abstraction.
+class VertexBuffer
+{
+public:
+    typedef std::shared_ptr<VertexBuffer> Ptr;
+
+public:
+    ~VertexBuffer (void);
+    static Ptr create (void);
+
+    void set_data (GLfloat const* data, GLsizei elems, GLint vpv);
+    void set_indices (GLuint const* data, GLsizei num_indices);
+
+    GLenum get_vbo_target (void) const;
+    GLenum get_data_type (void) const;
+    GLint get_values_per_vertex (void) const;
+    GLsizei get_element_amount (void) const;
+
+    void bind (void);
+
+private:
+    VertexBuffer (void);
+
+private:
+    GLuint vbo_id;
+    GLenum vbo_target;
+    GLenum datatype;
+    GLint vpv;
+    GLsizei elems;
+};
+
+inline
+VertexBuffer::~VertexBuffer (void)
+{
+    glDeleteBuffers(1, &this->vbo_id);
+    check_gl_error();
+}
+
+inline VertexBuffer::Ptr
+VertexBuffer::create (void)
+{
+    return Ptr(new VertexBuffer);
+}
+
+inline GLenum
+VertexBuffer::get_vbo_target (void) const
+{
+    return this->vbo_target;
+}
+
+inline GLint
+VertexBuffer::get_values_per_vertex (void) const
+{
+    return this->vpv;
+}
+
+inline GLsizei
+VertexBuffer::get_element_amount (void) const
+{
+    return this->elems;
+}
+
+inline GLenum
+VertexBuffer::get_data_type (void) const
+{
+    return this->datatype;
+}
+
+inline void
+VertexBuffer::bind (void)
+{
+    glBindBuffer(this->vbo_target, this->vbo_id);
+}
+
+/* ---- VertexArray ---- */
+
+// OpenGL vertex array object (VAO) abstraction.
+class VertexArray
+{
+public:
+    typedef std::shared_ptr<VertexArray> Ptr;
+
+    typedef std::pair<VertexBuffer::Ptr, std::string> BoundVBO;
+    typedef std::vector<BoundVBO> VBOList;
+
+public:
+    virtual ~VertexArray (void);
+
+    void set_primitive (GLuint primitive);
+    void set_shader (ShaderProgram::Ptr shader);
+    void set_vertex_vbo (VertexBuffer::Ptr vbo);
+    void set_index_vbo (VertexBuffer::Ptr vbo);
+    void add_vbo (VertexBuffer::Ptr vbo, std::string const& name);
+    void reset_vertex_array (void);
+    void draw (void);
+
+protected:
+    VertexArray (void);
+    void assign_attrib (BoundVBO const& bound_vbo);
+
+private:
+    GLuint vao_id;
+    GLuint primitive;
+    ShaderProgram::Ptr shader;
+
+    VertexBuffer::Ptr vert_vbo;
+    VertexBuffer::Ptr index_vbo;
+    VBOList vbo_list;
+};
+
+inline
+VertexArray::VertexArray (void)
+{
+    glGenVertexArrays(1, &this->vao_id);
+    check_gl_error();
+    this->primitive = GL_TRIANGLES;
+}
+
+inline
+VertexArray::~VertexArray (void)
+{
+    glDeleteVertexArrays(1, &this->vao_id);
+    check_gl_error();
+}
+
+inline void
+VertexArray::set_primitive (GLuint primitive)
+{
+    this->primitive = primitive;
+}
+
+inline void
+VertexArray::set_vertex_vbo (VertexBuffer::Ptr vbo)
+{
+    this->vert_vbo = vbo;
+}
+
+inline void
+VertexArray::set_index_vbo (VertexBuffer::Ptr vbo)
+{
+    this->index_vbo = vbo;
+}
+
+inline void
+VertexArray::add_vbo (VertexBuffer::Ptr vbo, std::string const& name)
+{
+    this->vbo_list.push_back(std::make_pair(vbo, name));
+}
+
+inline void
+VertexArray::reset_vertex_array(void)
+{
+    this->vert_vbo.reset();
+    this->index_vbo.reset();
+    this->vbo_list.clear();
+    glDeleteVertexArrays(1, &this->vao_id);
+    check_gl_error();
+    glGenVertexArrays(1, &this->vao_id);
+    check_gl_error();
+}
+
+inline void
+VertexArray::set_shader (ShaderProgram::Ptr shader)
+{
+    this->shader = shader;
+}
+
+/* ---- MeshRenderer ---- */
+
+// Takes a TriangleMesh and creates VBOs for rendering.
 class MeshRenderer : public VertexArray
 {
 public:
@@ -38,8 +207,6 @@ private:
     MeshRenderer (void);
     MeshRenderer (mve::TriangleMesh::ConstPtr mesh);
 };
-
-/* ---------------------------------------------------------------- */
 
 inline MeshRenderer::Ptr
 MeshRenderer::create (void)
