54978c6620f6e46d8161e8605c4f74fe42f184ce
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-03-19 14:44:51 -0700

    Move SceneManager inline methods from .h to .cc

--- apps/umve/SceneManager.cc
+++ apps/umve/SceneManager.cc
@@ -12,3 +12,25 @@ SceneManager& SceneManager::get(void) {
   static SceneManager instance;
   return instance;
 }
+
+void SceneManager::select_scene(sfm::Scene::Ptr scene) {
+  this->scene = scene;
+  emit this->scene_selected(scene);
+}
+
+void SceneManager::select_view(sfm::View::Ptr view) {
+  this->view = view;
+  emit this->view_selected(view);
+}
+
+sfm::Scene::Ptr SceneManager::get_scene(void) {
+  return this->scene;
+}
+
+void SceneManager::reset_scene(void) {
+  this->select_scene(sfm::Scene::Ptr());
+}
+
+void SceneManager::reset_view(void) {
+  this->select_view(sfm::View::Ptr());
+}
--- apps/umve/SceneManager.h
+++ apps/umve/SceneManager.h
@@ -27,33 +27,9 @@ public:
 
   void select_scene(sfm::Scene::Ptr scene);
   void select_view(sfm::View::Ptr view);
-
   sfm::Scene::Ptr get_scene(void);
-
   void reset_scene(void);
   void reset_view(void);
 };
-
-inline void SceneManager::select_scene(sfm::Scene::Ptr scene) {
-  this->scene = scene;
-  emit this->scene_selected(scene);
-}
-
-inline void SceneManager::select_view(sfm::View::Ptr view) {
-  this->view = view;
-  emit this->view_selected(view);
-}
-
-inline sfm::Scene::Ptr SceneManager::get_scene(void) {
-  return this->scene;
-}
-
-inline void SceneManager::reset_scene(void) {
-  this->select_scene(sfm::Scene::Ptr());
-}
-
-inline void SceneManager::reset_view(void) {
-  this->select_view(sfm::View::Ptr());
-}
 
 #endif // SFM_SCENE_MANAGER_HEADER
